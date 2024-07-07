03f060271a5aff11606ced059b3004443e1f15dd
JoaoRSS <joaors987654321@gmail.com>
2024-07-06 22:14:19 -0300

    Arumado toda a identadura e aplicação finalizada

--- webApp.js
+++ webApp.js
@@ -1,3 +1,6 @@
+
+    // Chaves para criptografar e descriptografar //
+
 const charEncript = {
     "a": "ai",
     "e": "enter",
@@ -14,6 +17,8 @@ const charDecript = {
     "ufat": "u"
 };
 
+    // Função para mudar os elementos ao clicar nos botões "Criptografar" e "Descriptografar" //
+
 function hiddenElements() {
     document.getElementById("decodificador-imagem").hidden = true;
     document.getElementById("decodificador-paragrafo").hidden = true;
@@ -22,49 +27,53 @@ function hiddenElements() {
     document.getElementById("decodificador-painel").removeAttribute("hidden");
     let editStyle = document.getElementById("descodificador").style;
     editStyle.justifyContent = "space-evenly";
-
 };
 
+    // Funcão para validar se o texto que o script irá trabalhar não a acentos ou maiúsculas //
+
+function testeValido(textoAvaliar) {
+    let testeMaiusculas = textoAvaliar.match(/[A-Z]/g);
+    let testeAcento = textoAvaliar.match(/[\u00C0-\u00FF]/g);
+    let testeReal = testeMaiusculas + testeAcento;
+    return testeReal
+};
+
+    // Função para criptografar o texto dentro da primeira textarea //
 
 function Criptografar() {
 
     hiddenElements();
     let textoDescrpit = document.getElementById("codificador-textarea").value;
-    let testeMaiusculas = textoDescrpit.match(/[A-Z]/g);
-    let testeAcento = textoDescrpit.match(/[\u00C0-\u00FF]/g);
-    var testeRealEncrip = testeMaiusculas + testeAcento;
-    if (testeRealEncrip){
+    let testeRealEncrip = testeValido(textoDescrpit)
+    if (testeRealEncrip) {
         document.getElementById("decodificador-painel").innerHTML = "";
-        }
-    else{
+    } else {
         let Encriptado = textoDescrpit.replace(/[aeiou]/g, m => charEncript[m]);
         document.getElementById("decodificador-painel").innerHTML = Encriptado;
     }
-
 };
+
+    // Função para descriptografar o texto dentro da primeira textarea //
 
 function Descriptografar() {
 
     hiddenElements();
     let textoEncript = document.getElementById("codificador-textarea").value;
-    let testeMaiusculas = textoEncript.match(/[A-Z]/g);
-    let testeAcento = textoEncript.match(/[\u00C0-\u00FF]/g);
-    var testeRealDecrip = testeMaiusculas + testeAcento;
+    let testeRealDecrip = testeValido(textoEncript);
 
-    if (testeRealDecrip){
+    if (testeRealDecrip) {
         document.getElementById("decodificador-painel").innerHTML = "";
-        }
-    else{
+    } else {
         var Descriptado = textoEncript.replace(/ai|enter|imes|ober|ufat/g, function (m) {return charDecript[m]});
         document.getElementById("decodificador-painel").innerHTML = Descriptado;
-        }
-
+    }
 };
+
+    // Função para copiar o texto dentro da segunda textarea (criptografado ou não) //
 
 function Copiar() {
     var textoCopiado = document.getElementById("decodificador-painel");
     textoCopiado.select();
     textoCopiado.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(textoCopiado.value);
-
 };
